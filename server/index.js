3f251005232e0b3127a7b6d76d54552dc8e8a64c
umayer10-AI <mdumayer10@gmail.com>
2026-05-07 08:48:03 +0600

    add

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express()
 const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
-// mongodb+srv://server2:nrMfNcMWFj0MTOMv@cluster0.ywso1qs.mongodb.net/?appName=Cluster0
+const uri = `mongodb+srv://server2:nrMfNcMWFj0MTOMv@cluster0.ywso1qs.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } 
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req,res) => {
     res.send("Umayer Ahmad")
