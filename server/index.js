3c16956a556e12357931f28c26894cb58b9620d1
umayer10-AI <mdumayer10@gmail.com>
2026-05-07 08:30:12 +0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.send("Umayer Ahmad")
+})
+
+app.listen(port, () => {
+    console.log(`Port Is Running ${port}`)
+})
