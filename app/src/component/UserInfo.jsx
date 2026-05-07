fea1adec47b99c69a974c468e36e5fffa0dc2423
umayer10-AI <mdumayer10@gmail.com>
2026-05-07 10:50:13 +0600

    add

--- app/src/component/UserInfo.jsx
+++ app/src/component/UserInfo.jsx
@@ -1,4 +1,4 @@
-import { Button, Table } from '@heroui/react';
+import { AlertDialog, Button, Table } from '@heroui/react';
 import Link from 'next/link';
 import React from 'react';
 
@@ -28,7 +28,36 @@ const UserInfo = ({p}) => {
                             <Table.Cell>
                                 <Link href={`/user/${v._id}`}><Button variant='outline'>Details</Button></Link>
                                 <Link href={`/user/${v._id}`}><Button variant='outline'>Edit</Button></Link>
-                                <Button variant='danger'>Delete</Button>
+                                
+                                <AlertDialog>
+      <Button variant="danger">Delete</Button>
+      <AlertDialog.Backdrop>
+        <AlertDialog.Container>
+          <AlertDialog.Dialog className="sm:max-w-[400px]">
+            <AlertDialog.CloseTrigger />
+            <AlertDialog.Header>
+              <AlertDialog.Icon status="danger" />
+              <AlertDialog.Heading>Delete User permanently?</AlertDialog.Heading>
+            </AlertDialog.Header>
+            <AlertDialog.Body>
+              <p>
+                This will permanently delete <strong>{v.name}</strong> and all of its
+                data. This action cannot be undone.
+              </p>
+            </AlertDialog.Body>
+            <AlertDialog.Footer>
+              <Button slot="close" variant="tertiary">
+                Cancel
+              </Button>
+              <Button slot="close" variant="danger">
+                Confirm Delete
+              </Button>
+            </AlertDialog.Footer>
+          </AlertDialog.Dialog>
+        </AlertDialog.Container>
+      </AlertDialog.Backdrop>
+    </AlertDialog>
+
                             </Table.Cell>
                         </Table.Row>
                     ))
