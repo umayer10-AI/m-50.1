b27b626eddc9be6bded1cdf445fbbbc0e67c6ebe
umayer10-AI <mdumayer10@gmail.com>
2026-05-07 12:38:40 +0600

    add

---
+++ app/src/app/user/[id]/edit/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const page = async ({params}) => {
+
+    const
+
+    return (
+        <div>
+            <h2>User Update: </h2>
+        </div>
+    );
+};
+
+export default page;
