718b98ec426bd45db5ee5c563ee8bcf91686dee8
umayer10-AI <mdumayer10@gmail.com>
2026-05-07 12:47:32 +0600

    add

--- app/src/app/user/[id]/edit/page.jsx
+++ app/src/app/user/[id]/edit/page.jsx
@@ -1,4 +1,5 @@
 import { getId } from '@/lib/data';
+import { Button, Input, Label, TextField } from '@heroui/react';
 import React from 'react';
 
 const page = async ({params}) => {
@@ -9,7 +10,31 @@ const page = async ({params}) => {
 
     return (
         <div>
-            <h2>User Update: </h2>
+            <h2>User Update: {data.name}</h2>
+            <div className='w-1/2 mx-auto'>
+                <form className="flex flex-col gap-4">
+                              <TextField className="w-full" name="name" value={data?.name} type="text">
+                                <Label>Name</Label>
+                                <Input placeholder="Enter your name" />
+                              </TextField>
+                              <TextField className="w-full" name="email" value={data?.email} type="email">
+                                <Label>Email</Label>
+                                <Input placeholder="Enter your email" />
+                              </TextField>
+                              <TextField className="w-full" name="role" value={data?.role} type="text">
+                                <Label>Role</Label>
+                                <Input placeholder="Enter your role" />
+                              </TextField>
+            
+                          <div className='flex gap-2'>
+                            <Button slot="close" variant="secondary">
+                            Cancel
+                          </Button>
+                          <Button type="submit" slot="close">Update</Button>
+                          </div>
+            
+                            </form>
+            </div>
         </div>
     );
 };
