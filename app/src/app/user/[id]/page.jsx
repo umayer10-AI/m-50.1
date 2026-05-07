b9401454ae4e3eb90809f556de3a531b9accb294
umayer10-AI <mdumayer10@gmail.com>
2026-05-07 10:16:03 +0600

    add

---
+++ app/src/app/user/[id]/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const page = async ({params}) => {
+
+    const {id} = await params
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default page;
